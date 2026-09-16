e6a94ae8b9ff80b52b89050c04197cd1479868db
Luis Masuelli <luisfmasuelli@gmail.com>
2026-09-15 20:01:47 -0500

    Adding the Filter property for ListOptions.

--- types/collection.go
+++ types/collection.go
@@ -27,6 +27,9 @@ type ListOptions struct {
 
 	// Sort can be empty. Implementations should validate fields against an allowlist.
 	Sort []Sort
+
+	// Filter can be empty. Implementations should validate fields against an allowlist.
+	Filter FilterExpression
 }
 
 // The CollectionList interface supports methods to retrieve a single element or a page of elements.
