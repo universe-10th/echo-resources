444093c7aef954008cf568ffa2d773c8c392a21e
Luis Masuelli <luisfmasuelli@gmail.com>
2026-09-16 21:44:57 -0500

    Adding more errors.

--- types/errors.go
+++ types/errors.go
@@ -6,8 +6,22 @@ package types
 type ErrorCode uint16
 
 const (
+	// ErrBadRequest is used when the JSON parsing did not
+	// succeed, or when Content-Type is not application/json.
+	ErrBadRequest ErrorCode = 400
+
+	// ErrUnauthorized is used when the request is not authorized.
+	ErrUnauthorized ErrorCode = 401
+
+	// ErrForbidden is used when the request is forbidden.
+	ErrForbidden ErrorCode = 403
+
 	// ErrNotFound is used when an element was not found.
 	ErrNotFound ErrorCode = 404
+
+	// ErrNotAcceptable is used when the client does not have
+	// application/json in its Accept header.
+	ErrNotAcceptable ErrorCode = 406
 
 	// ErrConflict is used when a constraint is violated
 	// (e.g. a key already in use), when a singleton in
@@ -21,6 +35,9 @@ const (
 	// more than conflict errors.
 	ErrInvalid ErrorCode = 422
 
+	// ErrThrottled is used when there were too many requests.
+	ErrThrottled ErrorCode = 429
+
 	// ErrInternal depicts an arbitrary error. Its details
 	// are never meant to reach the end user.
 	ErrInternal ErrorCode = 500
@@ -29,6 +46,49 @@ const (
 type Error interface {
 	error
 	Code() ErrorCode
+}
+
+// BadRequestError stands for when the request does not include
+// a body with application/json content-type and valid JSON
+// contents in the body.
+type BadRequestError struct{}
+
+// Error implements the error interface in BadRequestError.
+func (e BadRequestError) Error() string {
+	return "bad request"
+}
+
+// Code implements the error code for the Error interface in BadRequestError.
+func (e BadRequestError) Code() ErrorCode {
+	return ErrBadRequest
+}
+
+// UnauthorizedError stands for when the request is not
+// authorized, and requires authorization.
+type UnauthorizedError struct{}
+
+// Error implements the error interface in UnauthorizedError.
+func (e UnauthorizedError) Error() string {
+	return "unauthorized"
+}
+
+// Code implements the error code for the Error interface in UnauthorizedError.
+func (e UnauthorizedError) Code() ErrorCode {
+	return ErrUnauthorized
+}
+
+// ForbiddenError stands for when the request is not
+// authorized, and requires authorization.
+type ForbiddenError struct{}
+
+// Error implements the error interface in ForbiddenError.
+func (e ForbiddenError) Error() string {
+	return "forbidden"
+}
+
+// Code implements the error code for the Error interface in ForbiddenError.
+func (e ForbiddenError) Code() ErrorCode {
+	return ErrForbidden
 }
 
 // NotFoundError stands for when an element does not exist.
@@ -46,6 +106,21 @@ func (e NotFoundError[IDT]) Error() string {
 // returning ErrNotFound.
 func (e NotFoundError[IDT]) Code() ErrorCode {
 	return ErrNotFound
+}
+
+// NotAcceptableError stands for when the request cannot be
+// fulfilled because the client does not accept what the
+// server can generate as output content.
+type NotAcceptableError struct{}
+
+// Error implements the error interface in NotAcceptableError.
+func (e NotAcceptableError) Error() string {
+	return "not acceptable"
+}
+
+// Code implements the error code for the Error interface in NotAcceptableError.
+func (e NotAcceptableError) Code() ErrorCode {
+	return ErrNotAcceptable
 }
 
 // NotDeletedError stands for when an element is not soft-deleted
@@ -96,6 +171,20 @@ func (e ValidationError) Code() ErrorCode {
 	return ErrInvalid
 }
 
+// ThrottledError stands for when the request cannot be
+// fulfilled since too many requests arrived from that host.
+type ThrottledError struct{}
+
+// Error implements the error interface in ThrottledError.
+func (e ThrottledError) Error() string {
+	return "too many requests"
+}
+
+// Code implements the error code for the Error interface in ThrottledError.
+func (e ThrottledError) Code() ErrorCode {
+	return ErrThrottled
+}
+
 // InternalError stands for when an internal / unexpected error occurs.
 type InternalError struct{}
 
@@ -110,8 +199,12 @@ func (e InternalError) Code() ErrorCode {
 }
 
 var (
+	_ Error = BadRequestError{}
+	_ Error = UnauthorizedError{}
+	_ Error = ForbiddenError{}
 	_ Error = NotFoundError[string]{}
 	_ Error = NotDeletedError{}
+	_ Error = NotAcceptableError{}
 	_ Error = AlreadyUsedError{
 		KeyFields: nil,
 		Values:    nil,
@@ -119,5 +212,6 @@ var (
 	_ Error = ValidationError{
 		Errors: nil,
 	}
+	_ Error = ThrottledError{}
 	_ Error = InternalError{}
 )
