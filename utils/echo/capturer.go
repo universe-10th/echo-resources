b60c39c3308f7bb4116d8f079fe8e277d636a19c
Luis Masuelli <luisfmasuelli@gmail.com>
2026-09-21 11:59:59 -0500

    Removing capturer test, redoing capturer.go.

--- utils/echo/capturer.go
+++ utils/echo/capturer.go
@@ -1,53 +1,18 @@
 package echo
 
 import (
-	"mime"
-
 	"github.com/labstack/echo/v4"
 	"github.com/universe-10th/echo-resources/types"
 )
 
-// An ElementCapturer is a function used to capture an input request body.
-// Typically, this capturer always expects JSON, and only for one element.
-type ElementCapturer[IDT comparable, RT types.Resource[IDT]] func(context echo.Context) (RT, error)
-
-// MakeBodyCapturer creates a capturer that requires an application/json request
-// body and binds it directly into one resource element.
-func MakeBodyCapturer[IDT comparable, RT types.Resource[IDT]]() ElementCapturer[IDT, RT] {
-	return func(context echo.Context) (RT, error) {
-		var element RT
-
-		contentType, _, err := mime.ParseMediaType(context.Request().Header.Get(echo.HeaderContentType))
-		if err != nil || contentType != echo.MIMEApplicationJSON {
-			return element, renderBadRequest(context)
-		}
-
-		if err := context.Bind(&element); err != nil {
-			return element, renderBadRequest(context)
-		}
-
-		return element, nil
+// Capture reads and returns a specific struct from the input body.
+func Capture[IT any](context echo.Context) (IT, error) {
+	var element IT
+	binder := func(v any) error { return (&echo.DefaultBinder{}).BindBody(context, v) }
+	if err := binder(element); err != nil {
+		return element, renderBadRequest(context)
 	}
-}
-
-// MakeMappedBodyCapturer creates a capturer that requires an application/json
-// request body, binds it into an input type, and maps it to one resource element.
-func MakeMappedBodyCapturer[IDT comparable, RT types.Resource[IDT], IT any](mapper func(*IT) RT) ElementCapturer[IDT, RT] {
-	return func(context echo.Context) (RT, error) {
-		var empty RT
-		var input IT
-
-		contentType, _, err := mime.ParseMediaType(context.Request().Header.Get(echo.HeaderContentType))
-		if err != nil || contentType != echo.MIMEApplicationJSON {
-			return empty, renderBadRequest(context)
-		}
-
-		if err := context.Bind(&input); err != nil {
-			return empty, renderBadRequest(context)
-		}
-
-		return mapper(&input), nil
-	}
+	return element, nil
 }
 
 func renderBadRequest(context echo.Context) error {
